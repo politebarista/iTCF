6d23411d287fbcb9fdcbac81fb69605268992dbd
Ilya <twid01gc@gmail.com>
2020-07-25 12:34:34 +0700

    added showing link on your profile after authorized

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const signupBtn = document.getElementById('sign-upBtn');
+const signinBtn = document.getElementById('sign-inBtn');
+const profile = document.getElementById('profile');
 
 function getcookie(a) {var b = new RegExp(a+'=([^;]){1,}');var c = b.exec(document.cookie);if(c) c = c[0].split('=');else return false;return c[1] ? c[1] : false;}
 
 var havecookie = getcookie("authorizedAs");
 if (!havecookie) {
     signupBtn.classList.remove("hide");
+    signinBtn.classList.remove("hide");
+    profile.classList.add("hide");
 }
 
 const signupWin = document.querySelector(".sign-up");
