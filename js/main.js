d88dd61291ad723424f0597b45d850ed957cae02
Ilya <twid01gc@gmail.com>
2020-07-25 12:01:22 +0700

    add removing buttons after authorized, but idk work it or not

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 const signupBtn = document.getElementById('sign-upBtn');
+
+function getCookie(authorizedAs) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+if (!getCookie) {
+    signupBtn.classList.add('show');
+}
+
 const signupWin = document.querySelector(".sign-up");
 
 signupBtn.addEventListener('click', check);
